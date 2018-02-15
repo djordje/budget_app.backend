3161b1adf305c0211290b5aa25a18522c3e0300f
Djordje Kovacevic <djordje.bigjoe@gmail.com>
2018-02-15 15:59:17 +0100

    Add Expense.GetPaginated operation

---
+++ test/budget_app/operations/expense/get_paginated_test.exs
@@ -0,0 +1,34 @@
+defmodule BudgetApp.Operations.Expense.GetPaginatedTest do
+  use BudgetAppWeb.ConnCase, async: true
+
+  alias BudgetApp.Operations.Expense.GetPaginated
+
+  test "returns paginated list" do
+    currencies = insert_list(20, :expense)
+    {:ok, results} = GetPaginated.exec(1, 10)
+    assert results.entries       == Enum.take(currencies, 10)
+    assert results.page_number   == 1
+    assert results.page_size     == 10
+    assert results.total_entries == 20
+    assert results.total_pages   == 2
+  end
+
+  test "returns empty list when no currencies in the DB" do
+    {:ok, results} = GetPaginated.exec(1)
+    assert results.entries       == []
+    assert results.page_number   == 1
+    assert results.page_size     == 10
+    assert results.total_entries == 0
+    assert results.total_pages   == 1
+  end
+
+  test "returns empty list when page number exceeds total entries" do
+    insert_list(10, :expense)
+    {:ok, results} = GetPaginated.exec(2)
+    assert results.entries       == []
+    assert results.page_number   == 2
+    assert results.page_size     == 10
+    assert results.total_entries == 10
+    assert results.total_pages   == 1
+  end
+end
